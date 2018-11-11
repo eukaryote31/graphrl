a5a8ae3a0c8be7be6cf17fd8ae1f353be9c0cf41
eukaryote <eukaryote31@gmail.com>
2018-11-10 18:13:47 -0700

    Add additional graph generator tests

--- tests/test_graphgen.py
+++ tests/test_graphgen.py
@@ -1,8 +1,22 @@
 from graphrl import graphgen
 
 
+def test_toygen():
+    gen = graphgen.ToyGraphDistribution()
+    g = gen.sample_graph()
+
+    assert len(g) == 4
+
+
 def test_erdosrenyigen():
     gen = graphgen.ErdosRenyiDistribution()
+    g = gen.sample_graph(50)
+
+    assert len(g) == 50
+
+
+def test_barabasialbertgen():
+    gen = graphgen.BarabasiAlbertDistribution()
     g = gen.sample_graph(50)
 
     assert len(g) == 50
